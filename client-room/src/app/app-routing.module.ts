d1d6b11cd6343f03b106fbf673045069a9b04e9e
Tiago Vieira Siqueira <tiagov@badesc.gov.br>
2021-03-18 12:01:03 -0300

    frontend

---
+++ client-room/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { RoomListComponent } from './room-list/room-list.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+  { path: 'rooms', component: RoomListComponent },
+  { path: 'add', component: CreateRoomComponent },
+  { path: 'update/:id', component: UpdateRoomComponent },
+  { path: 'details/:id', component: RoomDetailsComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
